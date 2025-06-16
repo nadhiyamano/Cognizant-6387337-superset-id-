9b4a151f9974f20c0a2c92044c00e5ae7830556f
nadhiyamano <160404383+nadhiyamano@users.noreply.github.com>
2025-06-16 21:20:51 +0530

    Delete Cognizant-week1-Design Patterns and Principles - ex1-6/exercise 1-6/Implementing_the_Decorator_Pattern directory

--- Cognizant-week1-Design Patterns and Principles - ex1-6/exercise 1-6/Implementing_the_Decorator_Pattern/EmailNotifier.java
+++
@@ -1,8 +0,0 @@
-package Implementing_the_Decorator_Pattern;
-
-public class EmailNotifier implements Notifier {
-    @Override
-    public void send(String message) {
-        System.out.println("Sending Email: " + message);
-    }
-}
--- Cognizant-week1-Design Patterns and Principles - ex1-6/exercise 1-6/Implementing_the_Decorator_Pattern/Notifier.java
+++
@@ -1,6 +0,0 @@
-package Implementing_the_Decorator_Pattern;
-
-public interface Notifier {
-    void send(String message);
-}
-
