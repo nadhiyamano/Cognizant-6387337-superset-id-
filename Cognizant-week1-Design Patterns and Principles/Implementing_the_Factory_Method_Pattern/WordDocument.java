f61296e69ab1a720a4c786fc1aac784efac54fda
nadhiyamano <160404383+nadhiyamano@users.noreply.github.com>
2025-06-16 21:28:36 +0530

    Add files via upload

---
+++ Cognizant-week1-Design Patterns and Principles/Implementing_the_Factory_Method_Pattern/WordDocument.java
@@ -0,0 +1,9 @@
+package Implementing_the_Factory_Method_Pattern;
+
+public class WordDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening Word Document");
+    }
+}
+
