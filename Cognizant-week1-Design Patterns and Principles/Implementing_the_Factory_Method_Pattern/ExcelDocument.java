61eca5cfd3bcafb24bd743a647f5689b8c52e9f3
nadhiyamano <160404383+nadhiyamano@users.noreply.github.com>
2025-06-21 23:20:20 +0530

    Delete Cognizant-week1-Design Patterns and Principles directory

--- Cognizant-week1-Design Patterns and Principles/Implementing_the_Factory_Method_Pattern/ExcelDocument.java
+++
@@ -1,9 +0,0 @@
-package Implementing_the_Factory_Method_Pattern;
-
-public class ExcelDocument implements Document {
-    @Override
-    public void open() {
-        System.out.println("Opening Excel Document");
-    }
-}
-
