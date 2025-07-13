b84fe2a0c6816495c7818b13a1d3608bbf25ff6a
nadhiyamano <160404383+nadhiyamano@users.noreply.github.com>
2025-07-13 12:57:46 +0530

    Add files via upload

---
+++ Cognizant-week4-Mandatory-Spring REST using Spring Boot- JWT/Mandatory-5. JWT-handson/Create authentication service that returns JWT/jwt-auth-springboot/jwt-auth-springboot/src/main/java/com/example/jwtauth/AuthController.java
@@ -0,0 +1,23 @@
+package com.example.jwtauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final JwtUtil jwtUtil;
+
+    public AuthController(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @GetMapping
+    public String authenticate(@AuthenticationPrincipal User user) {
+        return jwtUtil.generateToken(user.getUsername());
+    }
+}
